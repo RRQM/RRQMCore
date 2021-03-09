55451e6173110ab5f7f5ae884713f42c6cfc7c4a
若汝棋茗 <76547834+RRQM@users.noreply.github.com>
2021-03-09 16:42:22 +0800

    新版本测试

---
+++ RRQMCore/Log/LogType.cs
@@ -0,0 +1,39 @@
+//------------------------------------------------------------------------------
+//  此代码版权归作者本人若汝棋茗所有
+//  源代码使用协议遵循本仓库的开源协议，若本仓库没有设置，则按MIT开源协议授权
+//  CSDN博客：https://blog.csdn.net/qq_40374647
+//  哔哩哔哩视频：https://space.bilibili.com/94253567
+//  源代码仓库：https://gitee.com/RRQM_Home
+//  交流QQ群：234762506
+//  感谢您的下载和使用
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRQMCore.Log
+{
+    /// <summary>
+    /// 日志类型
+    /// </summary>
+    public enum LogType
+    {
+        /// <summary>
+        /// 一般消息
+        /// </summary>
+        Message,
+
+        /// <summary>
+        /// 警示消息
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// 错误消息
+        /// </summary>
+        Error
+    }
+}
