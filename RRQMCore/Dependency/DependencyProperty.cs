f6c291b93aa2ec4f2fd17af279f914622210f4f8
向晚 <945180272@qq.com>
2021-05-31 16:59:32 +0800

    增加依赖项属性框架

---
+++ RRQMCore/Dependency/DependencyProperty.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRQMCore.Dependency
+{
+    /// <summary>
+    /// 依赖项属性
+    /// </summary>
+    public class DependencyProperty
+    {
+        private DependencyProperty()
+        {
+
+        }
+        private string name;
+
+        /// <summary>
+        /// 属性名
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
+
+        private Type owner;
+        /// <summary>
+        /// 所属类型
+        /// </summary>
+        public Type Owner
+        {
+            get { return owner; }
+        }
+
+        private Type valueType;
+        /// <summary>
+        /// 值类型
+        /// </summary>
+        public Type ValueType
+        {
+            get { return valueType; }
+        }
+
+        private object value;
+        /// <summary>
+        /// 默认值
+        /// </summary>
+        public object DefauleValue
+        {
+            get { return value; }
+        }
+
+        internal void SetValue(object value)
+        {
+            if (valueType.IsAssignableFrom(value.GetType()))
+            {
+                this.value = value;
+            }
+            else
+            {
+                throw new Exception("赋值类型与注册类型不一致");
+            }
+        }
+
+        /// <summary>
+        /// 注册依赖项属性
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="valueType"></param>
+        /// <param name="owner"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DependencyProperty Register(string propertyName, Type valueType, Type owner, object value)
+        {
+            DependencyProperty dp = new DependencyProperty();
+            dp.name = propertyName;
+            dp.valueType = valueType;
+            dp.owner = owner;
+            dp.SetValue(value);
+            return dp;
+        }
+    }
+}
---
+++ RRQMCore/Dependency/RRQMDependencyObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRQMCore.Dependency
+{
+    /// <summary>
+    /// 依赖项对象
+    /// </summary>
+   public class RRQMDependencyObject
+    {
+        private ConcurrentDictionary<DependencyProperty, object> dp = new ConcurrentDictionary<DependencyProperty, object>();
+
+        /// <summary>
+        /// 获取值
+        /// </summary>
+        /// <param name="dependencyProperty"></param>
+        /// <returns></returns>
+        public object GetValue(DependencyProperty dependencyProperty)
+        {
+            if (dp.TryGetValue(dependencyProperty, out object value))
+            {
+                return value;
+            }
+            else
+            {
+                return dependencyProperty.DefauleValue;
+            }
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <param name="dependencyProperty"></param>
+        /// <param name="value"></param>
+        public void SetValue(DependencyProperty dependencyProperty, object value)
+        {
+            dp.AddOrUpdate(dependencyProperty, value, (DependencyProperty dp, object v) =>
+            {
+                dp.SetValue(value);
+                return value;
+            });
+        }
+    }
+}
